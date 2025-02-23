7ac7d774e066bbb5bfc6c10c616af32b2c3ba1b4
雾野千里 <1535673062@qq.com>
2025-02-23 22:06:30 +0800

    home

---
+++ src/app/ER/page.tsx
@@ -0,0 +1,7 @@
+export default async function Page() {
+    return (
+      <div className="flex flex-col items-center justify-center">
+          <h1 className="text-3xl font-bold">This is ER Page</h1>
+      </div>
+    );
+  }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      hhh
+    <div className="flex flex-col items-center justify-center">
+      <Link href={"/EV"} key={"EV"}>
+      <div className="mb-8"><Image src={"/EV.png"} alt="EV" width={1000} height={760} /></div>
+      </Link>
+      <Link href={"/ER"} key={"ER"}>
+      <div><Image src={"/ER.png"} alt="ER" width={1000} height={760} /></div>
+      </Link>
     </div>
   );
 }
