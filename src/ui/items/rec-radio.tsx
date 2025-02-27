eef540c8d2b6649d65a6e9411d0eb59e309caac2
雾野千里 <1535673062@qq.com>
2025-02-28 01:03:20 +0800

    rec-radio

--- src/ui/items/rec-radio.tsx
+++ src/ui/items/rec-radio.tsx
@@ -2,7 +2,7 @@ import { RecRadioProps } from "../../lib/definitions";
 
 export default function RecRadio(info: RecRadioProps) {
     return (
-        <div className="flex flex-col border-2 border-[#000000] rounded-lg p-4">
+        <div className="flex flex-col border border-[#000000] rounded-lg p-4 mt-4">
             <p className="text-xl font-bold">{info.title}</p>
             <p className="text-lg">{info.description}</p>
         </div>
--- src/ui/parts/version.tsx
+++ src/ui/parts/version.tsx
@@ -1,10 +1,18 @@
 import RecRadio from "../items/rec-radio";
+import { RecRadioProps } from "../../lib/definitions";
+
+export const version: RecRadioProps[] = [
+    { title: "v1.0.0", description: "2023/08/01" },
+    { title: "v1.0.1", description: "2023/08/02" }, 
+]
 
 export default function Version() {
     return (
         <div className="flex flex-col">
             <p className="text-2xl m-4">版本</p>
-            <RecRadio {...{ title: "v1.0.0", description: "2023/08/01" }} />
+            {version.map((item, index) => {
+                return <RecRadio key={index} {...item} /> 
+            })}
         </div>
     );
 }
